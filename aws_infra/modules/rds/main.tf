2976d018b27ca622a231515616bf46f0470bad38
evangelostsak <baggtsak55@gmail.com>
2025-10-03 00:09:02 +0200

    add: implement RDS module with primary and read replica instances

---
+++ aws_infra/modules/rds/main.tf
@@ -0,0 +1,44 @@
+############################################
+# RDS MODULE
+############################################
+
+resource "aws_db_subnet_group" "this" {
+	name       = "${var.name_prefix}-db-subnet-group"
+	subnet_ids = var.private_subnet_ids
+	tags = { Name = "${var.name_prefix}-db-subnets" }
+}
+
+resource "aws_db_instance" "primary" {
+	allocated_storage       = var.allocated_storage
+	storage_type            = var.storage_type
+	engine                  = "postgres"
+	engine_version          = var.engine_version
+	instance_class          = var.db_class
+	identifier              = var.db_name
+	username                = var.db_user
+	password                = var.db_pass
+	skip_final_snapshot     = var.skip_final_snapshot
+	multi_az                = var.multi_az
+	backup_retention_period = var.backup_retention_period
+	monitoring_interval     = var.monitoring_interval
+	monitoring_role_arn     = var.monitoring_role_arn
+	db_subnet_group_name    = aws_db_subnet_group.this.name
+	publicly_accessible     = false
+	vpc_security_group_ids  = [var.rds_security_group_id]
+}
+
+resource "aws_db_instance" "read_replica" {
+	count                   = var.create_read_replica ? 1 : 0
+	identifier              = "${var.db_name}-replica"
+	instance_class          = var.db_class
+	engine                  = "postgres"
+	replicate_source_db     = aws_db_instance.primary.identifier
+	backup_retention_period = var.backup_retention_period
+	skip_final_snapshot     = var.skip_final_snapshot
+	publicly_accessible     = false
+	monitoring_interval     = var.monitoring_interval
+	monitoring_role_arn     = var.monitoring_role_arn
+	availability_zone       = var.read_replica_az
+	depends_on              = [aws_db_instance.primary]
+}
+
