241fee5de02a25f5a9ca19f4ad942b13b4764f1d
evangelostsak <baggtsak55@gmail.com>
2025-10-03 00:05:33 +0200

    add variables for EC2 instance configuration and user data

--- aws_infra/modules/ec2/variables.tf
+++ aws_infra/modules/ec2/variables.tf
@@ -10,6 +10,62 @@ variable "instance_type" {
 }
 
 variable "key_name" {
-  description = "The key name to use for the EC2 instance"
-  type        = string
+	type = string
 }
+
+variable "security_group_id" {
+	type = string
+}
+
+variable "subnet_ids" {
+	type = list(string)
+}
+
+variable "target_group_arn" {
+	type = string
+}
+
+variable "instance_profile_name" {
+	type = string
+}
+
+variable "min_size" {
+	type    = number
+	default = 3
+}
+
+variable "max_size" {
+	type    = number
+	default = 3
+}
+
+variable "desired_capacity" {
+	type    = number
+	default = 3
+}
+
+variable "health_check_type" {
+	type    = string
+	default = "EC2"
+}
+
+variable "health_check_grace_period" {
+	type    = number
+	default = 300
+}
+
+variable "user_data" {
+	type        = string
+	default     = ""
+	description = "If provided (already base64 if inline_user_data_base64=true) overrides default template"
+}
+variable "default_user_data_template_path" {
+	type    = string
+	default = "../templates/default_user_data.sh.tftpl"
+}
+
+variable "inline_user_data_base64" {
+	type    = bool
+	default = false
+}
+
