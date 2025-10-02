461097fa9a430093ee98e91d14333fedfba81f32
evangelostsak <baggtsak55@gmail.com>
2025-10-03 00:03:40 +0200

    add IAM module: define roles, policies, and instance profile for EC2 and RDS monitoring

---
+++ aws_infra/modules/iam/main.tf
@@ -0,0 +1,62 @@
+############################################
+# IAM MODULE - Roles, Policies, Instance Profile
+############################################
+
+resource "aws_iam_role" "ec2_role" {
+	name = "${var.name_prefix}-ec2-role"
+	assume_role_policy = jsonencode({
+		Version = "2012-10-17"
+		Statement = [{
+			Effect = "Allow"
+			Principal = { Service = "ec2.amazonaws.com" }
+			Action = "sts:AssumeRole"
+		}]
+	})
+}
+
+resource "aws_iam_policy" "s3_access" {
+	name        = "${var.name_prefix}-s3-access"
+	description = "Allow EC2 instances to access application S3 bucket"
+	policy = jsonencode({
+		Version = "2012-10-17",
+		Statement = [
+			{
+				Effect = "Allow",
+				Action = ["s3:GetObject", "s3:PutObject", "s3:ListBucket"],
+				Resource = [
+					var.s3_bucket_arn,
+					"${var.s3_bucket_arn}/*"
+				]
+			}
+		]
+	})
+}
+
+resource "aws_iam_role_policy_attachment" "attach_s3" {
+	role       = aws_iam_role.ec2_role.name
+	policy_arn = aws_iam_policy.s3_access.arn
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+	name = "${var.name_prefix}-ec2-instance-profile"
+	role = aws_iam_role.ec2_role.name
+}
+
+# RDS Enhanced Monitoring Role
+resource "aws_iam_role" "rds_monitoring" {
+	name = "${var.name_prefix}-rds-monitoring-role"
+	assume_role_policy = jsonencode({
+		Version = "2012-10-17"
+		Statement = [{
+			Effect = "Allow"
+			Principal = { Service = "monitoring.rds.amazonaws.com" }
+			Action = "sts:AssumeRole"
+		}]
+	})
+}
+
+resource "aws_iam_role_policy_attachment" "rds_monitoring_policy" {
+	role       = aws_iam_role.rds_monitoring.name
+	policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
+}
+
