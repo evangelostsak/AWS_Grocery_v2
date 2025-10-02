c6d6b916b4bd9717f44ac5b228125238a1e24871
evangelostsak <baggtsak55@gmail.com>
2025-10-03 00:07:38 +0200

    add: define variables for ALB module including health check settings and timeouts

---
+++ aws_infra/modules/alb/variables.tf
@@ -0,0 +1,56 @@
+variable "name_prefix" {
+	type = string
+}
+
+variable "vpc_id" {
+	type = string
+}
+
+variable "subnet_ids" {
+	type = list(string)
+}
+
+variable "security_group_id" {
+	type = string
+}
+
+variable "target_port" {
+	type    = number
+	default = 5000
+}
+
+variable "idle_timeout" {
+	type    = number
+	default = 60
+}
+
+variable "health_check_path" {
+	type    = string
+	default = "/"
+}
+
+variable "health_check_matcher" {
+	type    = string
+	default = "200"
+}
+
+variable "health_check_interval" {
+	type    = number
+	default = 15
+}
+
+variable "health_check_timeout" {
+	type    = number
+	default = 5
+}
+
+variable "health_check_healthy_threshold" {
+	type    = number
+	default = 2
+}
+
+variable "health_check_unhealthy_threshold" {
+	type    = number
+	default = 2
+}
+
