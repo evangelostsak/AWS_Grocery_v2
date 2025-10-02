89a2ab2f1a7a77c72c06ea90c54e316aa23b1f10
evangelostsak <baggtsak55@gmail.com>
2025-10-03 00:11:00 +0200

    add: implement security module with security groups and rules for ALB, EC2, and RDS

---
+++ aws_infra/modules/security/main.tf
@@ -0,0 +1,101 @@
+############################################
+# SECURITY MODULE - Security Groups & Rules
+############################################
+
+resource "aws_security_group" "alb" {
+	name        = "${var.name_prefix}-alb-sg"
+	description = "ALB security group"
+	vpc_id      = var.vpc_id
+	tags = {
+		Name = "${var.name_prefix}-alb-sg"
+	}
+}
+
+resource "aws_security_group" "ec2" {
+	name        = "${var.name_prefix}-ec2-sg"
+	description = "EC2/ASG security group"
+	vpc_id      = var.vpc_id
+	tags = {
+		Name = "${var.name_prefix}-ec2-sg"
+	}
+}
+
+resource "aws_security_group" "rds" {
+	name        = "${var.name_prefix}-rds-sg"
+	description = "RDS security group"
+	vpc_id      = var.vpc_id
+	tags = {
+		Name = "${var.name_prefix}-rds-sg"
+	}
+}
+
+# ALB inbound HTTP
+resource "aws_security_group_rule" "alb_http_in" {
+	type              = "ingress"
+	from_port         = 80
+	to_port           = 80
+	protocol          = "tcp"
+	cidr_blocks       = ["0.0.0.0/0"]
+	security_group_id = aws_security_group.alb.id
+}
+
+# ALB egress to EC2 app port
+resource "aws_security_group_rule" "alb_to_ec2" {
+	type                     = "egress"
+	from_port                = var.app_port
+	to_port                  = var.app_port
+	protocol                 = "tcp"
+	source_security_group_id = aws_security_group.ec2.id
+	security_group_id        = aws_security_group.alb.id
+}
+
+# EC2 ingress from ALB
+resource "aws_security_group_rule" "ec2_from_alb" {
+	type                     = "ingress"
+	from_port                = var.app_port
+	to_port                  = var.app_port
+	protocol                 = "tcp"
+	source_security_group_id = aws_security_group.alb.id
+	security_group_id        = aws_security_group.ec2.id
+}
+
+# EC2 SSH
+resource "aws_security_group_rule" "ec2_ssh" {
+	type              = "ingress"
+	from_port         = 22
+	to_port           = 22
+	protocol          = "tcp"
+	cidr_blocks       = [var.allowed_ssh_cidr]
+	security_group_id = aws_security_group.ec2.id
+}
+
+# EC2 egress all
+resource "aws_security_group_rule" "ec2_all_out" {
+	type              = "egress"
+	from_port         = 0
+	to_port           = 0
+	protocol          = "-1"
+	cidr_blocks       = ["0.0.0.0/0"]
+	security_group_id = aws_security_group.ec2.id
+}
+
+# RDS ingress from EC2
+resource "aws_security_group_rule" "rds_from_ec2" {
+	type                     = "ingress"
+	from_port                = var.db_port
+	to_port                  = var.db_port
+	protocol                 = "tcp"
+	source_security_group_id = aws_security_group.ec2.id
+	security_group_id        = aws_security_group.rds.id
+}
+
+# RDS egress all
+resource "aws_security_group_rule" "rds_all_out" {
+	type              = "egress"
+	from_port         = 0
+	to_port           = 0
+	protocol          = "-1"
+	cidr_blocks       = ["0.0.0.0/0"]
+	security_group_id = aws_security_group.rds.id
+}
+
