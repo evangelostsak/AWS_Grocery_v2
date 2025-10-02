20c69dc2d2ce1c76dbbfde3062755251176342be
evangelostsak <baggtsak55@gmail.com>
2025-10-03 00:05:59 +0200

    refactor: update variable definitions for consistency and clarity

--- aws_infra/modules/ec2/variables.tf
+++ aws_infra/modules/ec2/variables.tf
@@ -1,12 +1,18 @@
+variable "name_prefix" {
+	type = string
+}
+
+variable "environment" {
+	type = string
+}
+
 variable "ami_id" {
-  description = "The AMI ID to use for the EC2 instance"
-  type        = string
+	type = string
 }
 
 variable "instance_type" {
-  description = "The type of instance to use"
-  type        = string
-  default     = "t3.micro"
+	type    = string
+	default = "t3.micro"
 }
 
 variable "key_name" {
