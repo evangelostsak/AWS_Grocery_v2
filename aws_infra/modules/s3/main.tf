71ecbe682e244601de6277d2ef1191a030ba8c2f
evangelostsak <baggtsak55@gmail.com>
2025-10-03 18:06:20 +0200

    refactor: update S3 module to use project_name and environment variables for bucket naming

--- aws_infra/modules/s3/main.tf
+++ aws_infra/modules/s3/main.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "this" {
 	bucket_prefix = var.bucket_prefix
 	force_destroy = var.force_destroy
 	tags = {
-		Name = "${var.bucket_prefix}-bucket"
+		Name = "${var.project_name}-${var.environment}-${var.bucket_prefix}-bucket"
 	}
 }
 
