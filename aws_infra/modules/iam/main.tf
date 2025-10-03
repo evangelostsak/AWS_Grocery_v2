b3d4cb21c06321432f5a3d60c316377bfc0ddfb2
evangelostsak <baggtsak55@gmail.com>
2025-10-04 01:58:22 +0200

    feat: add tags to EC2 instance profile for improved resource identification

--- aws_infra/modules/iam/main.tf
+++ aws_infra/modules/iam/main.tf
@@ -46,6 +46,9 @@ resource "aws_iam_role_policy_attachment" "attach_s3" {
 resource "aws_iam_instance_profile" "ec2_profile" {
 	name = "${var.project_name}-${var.environment}-ec2-instance-profile"
 	role = aws_iam_role.ec2_role.name
+	tags = merge(local.merged_tags, {
+		Name = "${var.project_name}-${var.environment}-ec2-instance-profile"
+	})
 }
 
 # RDS Enhanced Monitoring Role
