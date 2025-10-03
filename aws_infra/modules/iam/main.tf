eb5375aca8c0cfb192b4f9abbe2e1ae5b6dffa21
evangelostsak <baggtsak55@gmail.com>
2025-10-03 18:03:44 +0200

    refactor: update IAM module to use project_name and environment variables for resource naming

--- aws_infra/modules/iam/main.tf
+++ aws_infra/modules/iam/main.tf
@@ -3,7 +3,7 @@
 ############################################
 
 resource "aws_iam_role" "ec2_role" {
-	name = "${var.name_prefix}-ec2-role"
+	name = "${var.project_name}-${var.environment}-ec2-role"
 	assume_role_policy = jsonencode({
 		Version = "2012-10-17"
 		Statement = [{
@@ -15,7 +15,7 @@ resource "aws_iam_role" "ec2_role" {
 }
 
 resource "aws_iam_policy" "s3_access" {
-	name        = "${var.name_prefix}-s3-access"
+	name        = "${var.project_name}-${var.environment}-s3-access"
 	description = "Allow EC2 instances to access application S3 bucket"
 	policy = jsonencode({
 		Version = "2012-10-17",
@@ -38,13 +38,13 @@ resource "aws_iam_role_policy_attachment" "attach_s3" {
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
-	name = "${var.name_prefix}-ec2-instance-profile"
+	name = "${var.project_name}-${var.environment}-ec2-instance-profile"
 	role = aws_iam_role.ec2_role.name
 }
 
 # RDS Enhanced Monitoring Role
 resource "aws_iam_role" "rds_monitoring" {
-	name = "${var.name_prefix}-rds-monitoring-role"
+	name = "${var.project_name}-${var.environment}-rds-monitoring-role"
 	assume_role_policy = jsonencode({
 		Version = "2012-10-17"
 		Statement = [{
