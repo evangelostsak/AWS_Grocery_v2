c4f4b15bd0178b7bb921daca50ccb6f6ab9deab5
evangelostsak <baggtsak55@gmail.com>
2025-10-03 18:04:21 +0200

    refactor: update RDS module to use project_name and environment variables for resource naming

--- aws_infra/modules/rds/main.tf
+++ aws_infra/modules/rds/main.tf
@@ -3,9 +3,9 @@
 ############################################
 
 resource "aws_db_subnet_group" "this" {
-	name       = "${var.name_prefix}-db-subnet-group"
+	name       = "${var.project_name}-${var.environment}-db-subnet-group"
 	subnet_ids = var.private_subnet_ids
-	tags = { Name = "${var.name_prefix}-db-subnets" }
+	tags = { Name = "${var.project_name}-${var.environment}-db-subnets" }
 }
 
 resource "aws_db_instance" "primary" {
