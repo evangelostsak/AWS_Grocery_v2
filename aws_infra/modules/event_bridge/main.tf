090c84adcbcec6ee999388d67f2789aab2c3b52c
evangelostsak <baggtsak55@gmail.com>
2025-10-05 01:17:33 +0200

    terraform fmt

--- aws_infra/modules/event_bridge/main.tf
+++ aws_infra/modules/event_bridge/main.tf
@@ -3,22 +3,22 @@
 ############################################
 
 resource "aws_cloudwatch_event_rule" "s3_upload_event" {
-	name        = var.rule_name
-	description = "Trigger Step Function when SQL dump is uploaded"
-	event_pattern = jsonencode({
-		source      = ["aws.s3"]
-		detail-type = ["Object Created"]
-		detail = {
-			bucket = { name = [var.bucket_name] }
-			object = { key = [var.db_dump_s3_key] }
-		}
-	})
-	tags = merge(local.merged_tags, { Name = "${var.project_name}-${var.environment}-s3-dump-event" })
+  name        = var.rule_name
+  description = "Trigger Step Function when SQL dump is uploaded"
+  event_pattern = jsonencode({
+    source      = ["aws.s3"]
+    detail-type = ["Object Created"]
+    detail = {
+      bucket = { name = [var.bucket_name] }
+      object = { key = [var.db_dump_s3_key] }
+    }
+  })
+  tags = merge(local.merged_tags, { Name = "${var.project_name}-${var.environment}-s3-dump-event" })
 }
 
 resource "aws_cloudwatch_event_target" "step_function_trigger_s3" {
-	rule      = aws_cloudwatch_event_rule.s3_upload_event.name
-	target_id = "StepFunctionTriggerS3"
-	arn       = var.state_machine_arn
-	role_arn  = var.eventbridge_role_arn
+  rule      = aws_cloudwatch_event_rule.s3_upload_event.name
+  target_id = "StepFunctionTriggerS3"
+  arn       = var.state_machine_arn
+  role_arn  = var.eventbridge_role_arn
 }
--- aws_infra/modules/event_bridge/outputs.tf
+++ aws_infra/modules/event_bridge/outputs.tf
@@ -1,4 +1,4 @@
 output "event_rule_name" {
-    description = "Name of the EventBridge rule for S3 upload triggering Step Function"
-    value       = aws_cloudwatch_event_rule.s3_upload_event.name
+  description = "Name of the EventBridge rule for S3 upload triggering Step Function"
+  value       = aws_cloudwatch_event_rule.s3_upload_event.name
 }
