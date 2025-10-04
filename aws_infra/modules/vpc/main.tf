0a43e9e619ab7ff9f1ba2b1ab01487b938e08a12
evangelostsak <baggtsak55@gmail.com>
2025-10-04 03:23:59 +0200

    feat: add public and private route tables with associations and S3 VPC endpoint

--- aws_infra/modules/vpc/main.tf
+++ aws_infra/modules/vpc/main.tf
@@ -49,7 +49,8 @@ resource "aws_subnet" "private" {
 	})
 }
 
-resource "aws_route_table" "public" {
+# Public Route Table
+resource "aws_route_table" "public_rt" {
 	vpc_id = aws_vpc.this.id
 	route {
 		cidr_block = "0.0.0.0/0"
@@ -60,9 +61,48 @@ resource "aws_route_table" "public" {
 	})
 }
 
+# Public Route Table Associations
 resource "aws_route_table_association" "public" {
 	for_each       = aws_subnet.public
 	subnet_id      = each.value.id
 	route_table_id = aws_route_table.public.id
 }
 
+# Private Route Table
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.this.id
+
+  tags = merge(local.merged_tags, {
+    Name = "${var.project_name}-${var.environment}-private-rt"
+  })
+}
+
+# Associate Private Subnets with Private Route Table
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+# DB Subnet Group for RDS
+resource "aws_db_subnet_group" "this" {
+  name       = "${var.project_name}-${var.environment}-db-subnet-group"
+  subnet_ids = aws_subnet.private[*].id
+
+  tags = merge(local.merged_tags, {
+    Name = "${var.project_name}-${var.environment}-db-subnet-group"
+  })
+}
+
+# S3 VPC Gateway Endpoint
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.this.id
+  service_name      = "com.amazonaws.${var.aws_region}.s3"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = [aws_route_table.private_rt.id]
+
+  tags = merge(local.merged_tags, {
+    Name = "${var.project_name}-${var.environment}-s3-endpoint"
+  })
+}
