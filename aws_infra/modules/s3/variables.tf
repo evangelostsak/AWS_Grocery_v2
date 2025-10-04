84a165280c58adda8c32ea3fc47af9df63f15ca7
evangelostsak <baggtsak55@gmail.com>
2025-10-04 03:25:21 +0200

    feat: add variables for database dump, avatar files, and Lambda layer in S3 module

--- aws_infra/modules/s3/variables.tf
+++ aws_infra/modules/s3/variables.tf
@@ -26,3 +26,47 @@ variable "versioning_enabled" {
 	default = true
 }
 
+variable "db_dump_prefix" {
+    description = "Prefix for database dump files in the S3 bucket"
+    type        = string
+}
+
+variable "avatar_prefix" {
+    description = "Prefix for avatar files in the S3 bucket"
+    type        = string
+}
+
+variable "avatar_filename" {
+    description = "Avatar file name"
+    type        = string
+}
+
+variable "avatar_path" {
+    description = "Avatar file path"
+    type        = string
+}
+
+variable "layer_filename" {
+    description = "Layer file name"
+    type        = string
+}
+
+variable "layer_path" {
+    description = "Layer file path"
+    type        = string
+}
+
+variable "layer_prefix" {
+    description = "Prefix for Lambda layer files in the S3 bucket"
+    type        = string
+}
+
+variable "ec2_iam_role_arn" {
+    description = "ARN of the IAM role that EC2 instances will assume to access the S3 bucket"
+    type        = string
+}
+
+variable "lambda_iam_role_arn" {
+    description = "ARN of the IAM role that Lambda functions will assume to access the S3 bucket"
+    type        = string
+}
