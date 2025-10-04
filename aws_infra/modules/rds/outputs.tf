3fa2b67af485772ad8134bc877ea38ecf29a1c36
evangelostsak <baggtsak55@gmail.com>
2025-10-05 01:20:10 +0200

    terraform fmt

--- aws_infra/modules/rds/outputs.tf
+++ aws_infra/modules/rds/outputs.tf
@@ -1,15 +1,15 @@
 output "db_endpoint" {
-	description = "Primary RDS instance endpoint address"
-	value       = aws_db_instance.primary.address
+  description = "Primary RDS instance endpoint address"
+  value       = aws_db_instance.primary.address
 }
 
 output "db_identifier" {
-	description = "Primary RDS instance identifier"
-	value       = aws_db_instance.primary.id
+  description = "Primary RDS instance identifier"
+  value       = aws_db_instance.primary.id
 }
 
 output "read_replica_endpoint" {
-	description = "Read replica endpoint if created, else null"
-	value       = try(aws_db_instance.read_replica[0].address, null)
+  description = "Read replica endpoint if created, else null"
+  value       = try(aws_db_instance.read_replica[0].address, null)
 }
 
--- aws_infra/modules/rds/variables.tf
+++ aws_infra/modules/rds/variables.tf
@@ -1,87 +1,87 @@
 variable "project_name" {
-    description = "Project name used for naming resources"
-	type = string
+  description = "Project name used for naming resources"
+  type        = string
 }
 
 variable "environment" {
-	description = "Environment name (dev/staging/prod)"
-	type = string
+  description = "Environment name (dev/staging/prod)"
+  type        = string
 }
 
 variable "common_tags" {
-    type = map(string)
-    default = {}
+  type    = map(string)
+  default = {}
 }
 
 variable "db_name" {
-	type = string
+  type = string
 }
 
 variable "db_user" {
-	type = string
+  type = string
 }
 
 variable "db_pass" {
-	type = string
+  type = string
 }
 
 variable "db_class" {
-	type = string
+  type = string
 }
 
 variable "read_replica_az" {
-	type = string
+  type = string
 }
 
 variable "private_subnet_ids" {
-	type = list(string)
+  type = list(string)
 }
 
 variable "rds_security_group_id" {
-	type = string
+  type = string
 }
 
 variable "monitoring_role_arn" {
-	type = string
+  type = string
 }
 
 variable "allocated_storage" {
-	type    = number
-	default = 20
+  type    = number
+  default = 20
 }
 
 variable "storage_type" {
-	type    = string
-	default = "standard"
+  type    = string
+  default = "standard"
 }
 
 variable "engine_version" {
-	type    = string
-	default = "15.12"
+  type    = string
+  default = "15.12"
 }
 
 variable "skip_final_snapshot" {
-	type    = bool
-	default = true
+  type    = bool
+  default = true
 }
 
 variable "multi_az" {
-	type    = bool
-	default = true
+  type    = bool
+  default = true
 }
 
 variable "backup_retention_period" {
-	type    = number
-	default = 7
+  type    = number
+  default = 7
 }
 
 variable "monitoring_interval" {
-	type    = number
-	default = 60
+  type    = number
+  default = 60
 }
 
 variable "create_read_replica" {
-	type    = bool
-	default = true
+  type    = bool
+  default = true
 }
 
