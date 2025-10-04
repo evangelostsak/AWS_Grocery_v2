367b631bbff27239f8b6eaff82c363537fd938e0
evangelostsak <baggtsak55@gmail.com>
2025-10-05 01:19:37 +0200

    terraform fmt

--- aws_infra/modules/monitoring/main.tf
+++ aws_infra/modules/monitoring/main.tf
@@ -3,56 +3,56 @@
 ############################################
 
 resource "aws_sns_topic" "alerts" {
-	name = "${var.project_name}-${var.environment}-alerts"
-	tags = merge(local.merged_tags, {
-		Name = "${var.project_name}-${var.environment}-alerts"
-	})
+  name = "${var.project_name}-${var.environment}-alerts"
+  tags = merge(local.merged_tags, {
+    Name = "${var.project_name}-${var.environment}-alerts"
+  })
 }
 
 resource "aws_sns_topic_subscription" "email" {
-	topic_arn = aws_sns_topic.alerts.arn
-	protocol  = "email"
-	endpoint  = var.alert_email
+  topic_arn = aws_sns_topic.alerts.arn
+  protocol  = "email"
+  endpoint  = var.alert_email
 }
 
 resource "aws_cloudwatch_metric_alarm" "high_cpu" {
-	alarm_name          = "${var.project_name}-${var.environment}-high-cpu"
-	comparison_operator = "GreaterThanThreshold"
-	evaluation_periods  = 2
-	metric_name         = "CPUUtilization"
-	namespace           = "AWS/EC2"
-	period              = var.cpu_period
-	statistic           = "Average"
-	threshold           = var.cpu_threshold
-	alarm_description   = "High average CPU utilization"
-	alarm_actions       = [aws_sns_topic.alerts.arn]
-	ok_actions          = [aws_sns_topic.alerts.arn]
-	dimensions = { AutoScalingGroupName = var.asg_name }
-	tags = merge(local.merged_tags, {
-		Name = "${var.project_name}-${var.environment}-high-cpu"
-	})
+  alarm_name          = "${var.project_name}-${var.environment}-high-cpu"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = var.cpu_period
+  statistic           = "Average"
+  threshold           = var.cpu_threshold
+  alarm_description   = "High average CPU utilization"
+  alarm_actions       = [aws_sns_topic.alerts.arn]
+  ok_actions          = [aws_sns_topic.alerts.arn]
+  dimensions          = { AutoScalingGroupName = var.asg_name }
+  tags = merge(local.merged_tags, {
+    Name = "${var.project_name}-${var.environment}-high-cpu"
+  })
 }
 
 resource "aws_cloudwatch_metric_alarm" "disk_usage" {
-	alarm_name          = "${var.project_name}-${var.environment}-disk-usage"
-	comparison_operator = "GreaterThanThreshold"
-	evaluation_periods  = 2
-	metric_name         = "disk_used_percent"
-	namespace           = "CWAgent"
-	period              = var.disk_period
-	statistic           = "Average"
-	threshold           = var.disk_threshold
-	alarm_description   = "Disk usage percentage too high"
-	alarm_actions       = [aws_sns_topic.alerts.arn]
-	ok_actions          = [aws_sns_topic.alerts.arn]
-	dimensions = {
-		AutoScalingGroupName = var.asg_name
-		Filesystem           = var.disk_filesystem
-		MountPath            = var.disk_mount_path
-	}
-	tags = merge(local.merged_tags, {
-		Name = "${var.project_name}-${var.environment}-disk-usage"
-	})
+  alarm_name          = "${var.project_name}-${var.environment}-disk-usage"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "disk_used_percent"
+  namespace           = "CWAgent"
+  period              = var.disk_period
+  statistic           = "Average"
+  threshold           = var.disk_threshold
+  alarm_description   = "Disk usage percentage too high"
+  alarm_actions       = [aws_sns_topic.alerts.arn]
+  ok_actions          = [aws_sns_topic.alerts.arn]
+  dimensions = {
+    AutoScalingGroupName = var.asg_name
+    Filesystem           = var.disk_filesystem
+    MountPath            = var.disk_mount_path
+  }
+  tags = merge(local.merged_tags, {
+    Name = "${var.project_name}-${var.environment}-disk-usage"
+  })
 }
 
 ############################################
@@ -61,40 +61,40 @@ resource "aws_cloudwatch_metric_alarm" "disk_usage" {
 
 # Lambda log group
 resource "aws_cloudwatch_log_group" "lambda_log_group" {
-	name              = "/aws/lambda/db_populator"
-	retention_in_days = 7
-	tags = merge(local.merged_tags, { Name = "${var.project_name}-${var.environment}-lambda-log" })
+  name              = "/aws/lambda/db_populator"
+  retention_in_days = 7
+  tags              = merge(local.merged_tags, { Name = "${var.project_name}-${var.environment}-lambda-log" })
 }
 
 # Step Function log group
 resource "aws_cloudwatch_log_group" "step_function_log_group" {
-	name              = "/aws/vendedlogs/states/db-restore-step-function"
-	retention_in_days = 7
-	tags = merge(local.merged_tags, { Name = "${var.project_name}-${var.environment}-sfn-log" })
+  name              = "/aws/vendedlogs/states/db-restore-step-function"
+  retention_in_days = 7
+  tags              = merge(local.merged_tags, { Name = "${var.project_name}-${var.environment}-sfn-log" })
 }
 
 # Resource policy to allow Step Functions to write to CloudWatch Logs
 resource "aws_cloudwatch_log_resource_policy" "step_function_log_policy" {
-	policy_name     = "StepFunctionLogPolicy"
-	policy_document = jsonencode({
-		Version = "2012-10-17"
-		Statement = [
-			{
-				Effect = "Allow"
-				Principal = { Service = "states.amazonaws.com" }
-				Action = [
-					"logs:CreateLogGroup",
-					"logs:CreateLogStream",
-					"logs:PutLogEvents",
-					"logs:CreateLogDelivery",
-					"logs:GetLogDelivery",
-					"logs:UpdateLogDelivery",
-					"logs:DeleteLogDelivery",
-					"logs:ListLogDeliveries"
-				]
-				Resource = "arn:aws:logs:*:*:*"
-			}
-		]
-	})
+  policy_name = "StepFunctionLogPolicy"
+  policy_document = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "states.amazonaws.com" }
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:CreateLogDelivery",
+          "logs:GetLogDelivery",
+          "logs:UpdateLogDelivery",
+          "logs:DeleteLogDelivery",
+          "logs:ListLogDeliveries"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
 }
 
--- aws_infra/modules/monitoring/variables.tf
+++ aws_infra/modules/monitoring/variables.tf
@@ -1,62 +1,62 @@
 variable "project_name" {
-    description = "Project name used for naming resources"
-	type = string
+  description = "Project name used for naming resources"
+  type        = string
 }
 
 variable "environment" {
-	description = "Environment name (dev/staging/prod)"
-	type = string
+  description = "Environment name (dev/staging/prod)"
+  type        = string
 }
 
 variable "common_tags" {
-	description = "Additional user-specified tags to merge with enforced tags"
-	type        = map(string)
-	default     = {}
+  description = "Additional user-specified tags to merge with enforced tags"
+  type        = map(string)
+  default     = {}
 }
 
 variable "asg_name" {
-	description = "Name of the Auto Scaling Group whose metrics are monitored"
-	type        = string
+  description = "Name of the Auto Scaling Group whose metrics are monitored"
+  type        = string
 }
 
 variable "alert_email" {
-	description = "Email address subscribed to the SNS alerts topic"
-	type        = string
+  description = "Email address subscribed to the SNS alerts topic"
+  type        = string
 }
 
 variable "cpu_threshold" {
-	description = "Average CPU utilization (%) threshold to trigger alarm"
-	type        = number
-	default     = 80
+  description = "Average CPU utilization (%) threshold to trigger alarm"
+  type        = number
+  default     = 80
 }
 
 variable "cpu_period" {
-	description = "Evaluation period (seconds) for CPU metric"
-	type        = number
-	default     = 300
+  description = "Evaluation period (seconds) for CPU metric"
+  type        = number
+  default     = 300
 }
 
 variable "disk_threshold" {
-	description = "Disk usage percent threshold to trigger alarm"
-	type        = number
-	default     = 80
+  description = "Disk usage percent threshold to trigger alarm"
+  type        = number
+  default     = 80
 }
 
 variable "disk_period" {
-	description = "Evaluation period (seconds) for disk usage metric"
-	type        = number
-	default     = 300
+  description = "Evaluation period (seconds) for disk usage metric"
+  type        = number
+  default     = 300
 }
 
 variable "disk_filesystem" {
-	description = "Filesystem device name reported by CloudWatch Agent"
-	type        = string
-	default     = "/dev/xvda1"
+  description = "Filesystem device name reported by CloudWatch Agent"
+  type        = string
+  default     = "/dev/xvda1"
 }
 
 variable "disk_mount_path" {
-	description = "Mount path corresponding to the monitored filesystem"
-	type        = string
-	default     = "/"
+  description = "Mount path corresponding to the monitored filesystem"
+  type        = string
+  default     = "/"
 }
 
