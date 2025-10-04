45a22a35bc03345d65478f14b207200d9fda81f6
evangelostsak <baggtsak55@gmail.com>
2025-10-05 01:51:18 +0200

    terraform fmt

--- aws_infra/modules/ecr/main.tf
+++ aws_infra/modules/ecr/main.tf
@@ -3,7 +3,7 @@
 ############################################
 
 resource "aws_ecr_repository" "this" {
-  name                 = "${var.project_name}-${var.environment}-${var.repository_name}"
+  name = "${var.project_name}-${var.environment}-${var.repository_name}"
   tags = merge(local.merged_tags, {
     Name = "${var.project_name}-${var.environment}-${var.repository_name}"
   })
