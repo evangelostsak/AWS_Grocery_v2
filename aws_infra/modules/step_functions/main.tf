66bc93ab2e113c78984f4ffdcc691470890c9486
evangelostsak <baggtsak55@gmail.com>
2025-10-05 01:21:55 +0200

    terraform fmt

--- aws_infra/modules/step_functions/main.tf
+++ aws_infra/modules/step_functions/main.tf
@@ -3,19 +3,19 @@
 ############################################
 
 resource "aws_sfn_state_machine" "db_restore_sfn" {
-	name     = var.state_machine_name
-	role_arn = var.sfn_role_arn
+  name     = var.state_machine_name
+  role_arn = var.sfn_role_arn
 
-	definition = jsonencode(local.state_machine_definition)
+  definition = jsonencode(local.state_machine_definition)
 
-	logging_configuration {
-		log_destination        = "${var.step_function_log_group_arn}:*"
-		level                  = "ALL"
-		include_execution_data = true
-	}
+  logging_configuration {
+    log_destination        = "${var.step_function_log_group_arn}:*"
+    level                  = "ALL"
+    include_execution_data = true
+  }
 
-	tags = merge(local.merged_tags, {
-		Name = "${var.project_name}-${var.environment}-sfn-db-restore"
-		Type = "state-machine"
-	})
+  tags = merge(local.merged_tags, {
+    Name = "${var.project_name}-${var.environment}-sfn-db-restore"
+    Type = "state-machine"
+  })
 }
--- aws_infra/modules/step_functions/variables.tf
+++ aws_infra/modules/step_functions/variables.tf
@@ -1,54 +1,54 @@
 variable "project_name" {
-	description = "Project name for naming the state machine"
-	type        = string
+  description = "Project name for naming the state machine"
+  type        = string
 }
 
 variable "environment" {
-	description = "Deployment environment (dev/staging/prod)"
-	type        = string
+  description = "Deployment environment (dev/staging/prod)"
+  type        = string
 }
 
 variable "common_tags" {
-	type    = map(string)
-	default = {}
+  type    = map(string)
+  default = {}
 }
 
 variable "state_machine_name" {
-	description = "Name of the Step Functions state machine"
-	type        = string
+  description = "Name of the Step Functions state machine"
+  type        = string
 }
 
 variable "sfn_role_arn" {
-	description = "IAM role ARN assumed by Step Functions"
-	type        = string
+  description = "IAM role ARN assumed by Step Functions"
+  type        = string
 }
 variable "db_identifier" {
-	description = "RDS instance identifier to poll for availability"
-	type        = string
+  description = "RDS instance identifier to poll for availability"
+  type        = string
 }
 
 variable "bucket_name" {
-	description = "S3 bucket name containing the database dump"
-	type        = string
+  description = "S3 bucket name containing the database dump"
+  type        = string
 }
 
 variable "db_dump_s3_key" {
-	description = "S3 object key of the database dump file"
-	type        = string
+  description = "S3 object key of the database dump file"
+  type        = string
 }
 
 variable "lambda_function_arn" {
-	description = "ARN of the Lambda function to invoke once conditions are met"
-	type        = string
+  description = "ARN of the Lambda function to invoke once conditions are met"
+  type        = string
 }
 
 variable "wait_seconds" {
-	description = "Seconds to wait between S3 file existence checks"
-	type        = number
-	default     = 60
+  description = "Seconds to wait between S3 file existence checks"
+  type        = number
+  default     = 60
 }
 
 variable "step_function_log_group_arn" {
-	description = "CloudWatch Log Group ARN for state machine execution logging"
-	type        = string
+  description = "CloudWatch Log Group ARN for state machine execution logging"
+  type        = string
 }
