31517654ec993ea9b92f25a9d4bd49e6d8ec23c8
evangelostsak <baggtsak55@gmail.com>
2025-10-05 01:22:25 +0200

    terraform fmt

--- aws_infra/modules/vpc/variables.tf
+++ aws_infra/modules/vpc/variables.tf
@@ -1,39 +1,39 @@
 variable "project_name" {
-    description = "Project name used for naming resources"
-	type = string
+  description = "Project name used for naming resources"
+  type        = string
 }
 
 variable "environment" {
-	description = "Environment name (dev/staging/prod)"
-	type = string
+  description = "Environment name (dev/staging/prod)"
+  type        = string
 }
 
 variable "common_tags" {
-    type = map(string)
-    default = {}
+  type    = map(string)
+  default = {}
 }
 
 variable "vpc_cidr" {
-	description = "CIDR block for the VPC"
-	type        = string
+  description = "CIDR block for the VPC"
+  type        = string
 }
 
 variable "public_subnet_cidrs" {
-	description = "List of CIDR blocks for public subnets"
-	type        = list(string)
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
 }
 
 variable "private_subnet_cidrs" {
-	description = "List of CIDR blocks for private subnets"
-	type        = list(string)
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
 }
 
 variable "availability_zones" {
-	description = "List of availability zones aligned with subnet cidr indexes"
-	type        = list(string)
+  description = "List of availability zones aligned with subnet cidr indexes"
+  type        = list(string)
 }
 
 variable "aws_region" {
-    description = "AWS region for the VPC"
-    type        = string
+  description = "AWS region for the VPC"
+  type        = string
 }
